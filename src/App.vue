f9134a5462b89e6c31a10a08ff027b9cc7d38aaf
middle1204 <guu0414a@gmail.com>
2026-07-15 15:08:59 +0900

    add :: added Header & Footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <div id="app-wrapper">
+    <!-- 모든 페이지에 공통으로 보일 헤더 -->
+    <Header @open-write-modal="handleOpenModal" />
+
+    <!-- 라우팅되는 페이지 내용이 이 자리에 렌더링됩니다 -->
+    <router-view />
+
+    <!-- 모든 페이지에 공통으로 보일 푸터 -->
+    <Footer />
+
+    <!-- 글쓰기 모달 (전역으로 관리) -->
+    <div v-if="isModalOpen" class="fixed inset-0 ...">
+      <!-- 모달 내용 -->
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+
+const isModalOpen = ref(false);
+const handleOpenModal = () => {
+  isModalOpen.value = true;
+};
+</script>
---
+++ src/views/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <MainImage />
+    <BoardList />
+    <MapPreview />
+    <Graph />
+  </main>
+</template>
+
+<script setup>
+// 메인 페이지 전용 컴포넌트들만 import
+import MainImage from '../components/main/MainImage.vue';
+import BoardList from '../components/main/BoardList.vue';
+import MapPreview from '../components/main/MapPreview.vue';
+import Graph from '../components/main/Graph.vue';
+</script>
