e0bf5a772efffc0aecaa729fdf766d69f1624f25
middle1204 <guu0414a@gmail.com>
2026-07-16 15:47:22 +0900

    fix :: 대문자 수정

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -10,7 +10,7 @@
 <script setup>
 // 메인 페이지 전용 컴포넌트들만 import
 import MainImage from '../components/main/mainImage.vue';
-import BoardList from '../components/main/boardList.vue'; 
+import BoardList from '../components/main/boardlist.vue'; 
 import MapPreview from '../components/main/mapPreview.vue';
 import Graph from '../components/main/graph.vue';
 </script>
